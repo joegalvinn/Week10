25f60fe916b12e289655d6e8fb9e7114017089ed
Joseph <joseph.galvin@digisolve.co.uk>
2024-10-31 09:22:31 +0000

    update

---
+++ self-study-test-site/src/app/buttonPage/page.jsx
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+export default function Buttons() {
+  return (
+    <div>
+      <div className="header">
+        <p>Hello</p>
+
+        <button>Click me</button>
+      </div>
+      <div className="div2">
+        <button className="orme">Or me</button>
+        <button className="orme2">second button</button>
+      </div>
+    </div>
+  );
+}
